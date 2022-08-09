654c27b9f585799c20abd9e486b71c53139013cc
Ricardo Catalinas Jiménez <ricardo.catalinas@checkout.com>
2022-08-09 15:03:14 +0100

    Fix HTTP port inprocessing_channels/import.sh

--- _data/processing_channels/import.sh
+++ _data/processing_channels/import.sh
@@ -1,1 +1,1 @@
-for f in *.json; do curl -X POST -H 'Content-Type: application/json' https://localhost:7209/api/processing-channels --data @$f ; done
+for f in *.json; do curl -X POST -H 'Content-Type: application/json' http://localhost:5109/api/processing-channels --data @$f ; done
